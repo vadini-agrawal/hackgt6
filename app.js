a472784970789b5e908ef4e891d9eb8d17b4c27e
vagrawal48 <vadini@gatech.edu>
2019-10-26 14:07:03 -0400

    Added input box for location

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 function saveInput(){
-  var text_to_save=document.getElementById('businessName').value;
-  console.log(text_to_save);
-  localStorage.setItem("text", text_to_save); // save the item
-
-
+  var name_to_save=document.getElementById('businessName').value;
+  var loc_to_save=document.getElementById('businessLocation').value;
+  console.log(name_to_save);
+  console.log(loc_to_save);
+  localStorage.setItem("text", name_to_save); // save the item
+  localStorage.setItem("text", loc_to_save); // save the item
 }
 
 // function displayValues() {
